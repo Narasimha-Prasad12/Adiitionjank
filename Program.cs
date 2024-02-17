a361013a9f97852d2dbe9bbfc98edfe7c1ed4380
narasimha <narasimha@gmail.com>
2024-02-17 19:03:24 +0530

    Addition

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//public void Add(int a,int b)
+//{
+
+//}
+Console.WriteLine("Adding");
+int num1 = 1;
+int num2 = 3;
+Console.WriteLine("{0} + {1} = {2}",num1,num2,(num1+num2));
